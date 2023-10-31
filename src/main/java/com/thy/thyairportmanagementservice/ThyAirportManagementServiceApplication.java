ced2a1d1c27768f1a005ee3a823bc6cf1b20737f
Cemal Türkcan <101227219+cemalturkcan@users.noreply.github.com>
2023-10-31 15:44:05 +0300

    initial

---
+++ src/main/java/com/thy/thyairportmanagementservice/ThyAirportManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thy.thyairportmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThyAirportManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThyAirportManagementServiceApplication.class, args);
+	}
+
+}
